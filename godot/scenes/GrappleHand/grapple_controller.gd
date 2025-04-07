25a9e5a2db139cd244c47235b8b32fa39d090d8b
HugoEnzo <sivasankaranirudh@gmail.com>
2025-04-07 15:33:04 -0400

    almost done

--- godot/scenes/GrappleHand/grapple_controller.gd
+++ godot/scenes/GrappleHand/grapple_controller.gd
@@ -6,12 +6,20 @@ extends Node2D
 
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var player: RigidBody2D = $".."
-@onready var line_2d: Line2D = $Line2D
+@onready var male_arm: Sprite2D = $MaleArm
+
+var upright:bool = true
 
 const VELOCITY = 20.0
 
 var launched = false
 var target: Vector2
+var contact_point:Vector2
+
+func _integrate_forces(state) -> void:
+	if state.get_contact_count() > 0:
+		contact_point = state.get_contact_collider_position(0)
+
 
 func _ready() -> void:
 	#player.lock_rotation = true
@@ -19,12 +27,13 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	
 	ray_cast_2d.look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("mouse_grab"):
 		launch()
+		
 	if Input.is_action_just_released("mouse_grab"):
 		retract()
+		
 	
 	if launched:
 		handle_grapple(delta)
@@ -33,16 +42,17 @@ func launch():
 	if ray_cast_2d.is_colliding():
 		launched = true
 		target = ray_cast_2d.get_collision_point()
-	line_2d.show()
+	male_arm.show()
 
 func retract():
 	launched = false
-	line_2d.hide()
+	male_arm.hide()
 
 
 func handle_grapple(delta):
 	player.global_position = player.global_position.move_toward(Vector2(target.x, target.y), VELOCITY*delta)
 	
 
-func update_rope():
-	line_2d.set_point_position(1, to_local(target))
+func sprite_flip():
+	if player.position != contact_point:
+		false
