a2d66940a5d2ea91de87cf4b07e4f3455afaa766
HugoEnzo <sivasankaranirudh@gmail.com>
2025-04-07 21:25:49 -0400

    FinishedLD57, need water and controller fixes

--- godot/scenes/GrappleHand/grapple_controller.gd
+++ godot/scenes/GrappleHand/grapple_controller.gd
@@ -4,6 +4,12 @@ extends Node2D
 @export var stiffness = 10.0
 @export var damping = 2.0
 
+@export var caveCollider:PackedScene
+@onready var hand_grappler: Node2D = $"../.."
+@onready var collision_shape_2d: CollisionShape2D = $"../CollisionShape2D"
+
+
+
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var player: RigidBody2D = $".."
 @onready var male_arm: Sprite2D = $MaleArm
@@ -16,22 +22,23 @@ var launched = false
 var target: Vector2
 var contact_point:Vector2
 
-func _integrate_forces(state) -> void:
-	if state.get_contact_count() > 0:
-		contact_point = state.get_contact_collider_position(0)
+
 
 
 func _ready() -> void:
-	#player.lock_rotation = true
+	#caveCollider.
 	pass
 
 
 func _process(delta: float) -> void:
+	
 	ray_cast_2d.look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("mouse_grab"):
+		#sprite_flip()
 		launch()
 		
 	if Input.is_action_just_released("mouse_grab"):
+		#sprite_flip()
 		retract()
 		
 	
@@ -53,6 +60,8 @@ func handle_grapple(delta):
 	player.global_position = player.global_position.move_toward(Vector2(target.x, target.y), VELOCITY*delta)
 	
 
-func sprite_flip():
-	if player.position != contact_point:
-		false
+#func sprite_flip():
+	#if (player.global_position.x -get_global_mouse_position().x > 0):
+		#collision_shape_2d.scale.x = -1
+	#else:
+		#collision_shape_2d.scale.x = 1
