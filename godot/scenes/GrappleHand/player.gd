e762374dba829cd82d6fbfd5954d5a103c5f6229
HugoEnzo <sivasankaranirudh@gmail.com>
2025-04-17 22:23:03 -0400

    fixed the sprite flip mechanic

--- godot/scenes/GrappleHand/player.gd
+++ godot/scenes/GrappleHand/player.gd
@@ -1,14 +1,15 @@
 extends RigidBody2D
 
+@onready var image_1: Sprite2D = $Image1
 
 func _physics_process(delta: float) -> void:
-	#sprite_flip()
+	sprite_flip()
 	pass
 
 
 func sprite_flip():
 	var mouse_position = get_global_mouse_position()
 	if mouse_position.x > global_position.x:
-		scale.x = -1
+		image_1.flip_h = false
 	else:
-		scale.x = 1
+		image_1.flip_h = true
